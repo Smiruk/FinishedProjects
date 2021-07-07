bbba459e45902f970e092f5f6bc411f96ddfcf26
Smiruk <87028279+Smiruk@users.noreply.github.com>
2021-07-07 18:29:50 +0300

    Update usdtorub.py

--- usdtorub.py
+++ usdtorub.py
@@ -1,6 +1,6 @@
 import bs4, requests
 
-def convertRUBToUSD(amount):
+def convertUSDToRUB(amount):
     res = requests.get('https://finance.yahoo.com/quote/RUB=X/') #Обращается к ресурсу
     USD = bs4.BeautifulSoup(res.text, 'html.parser') #парсит html ресурса
     USDPrice = USD.select('span.Fw\(b\):nth-child(1)') #узнаёт курс доллара на ресурсе
@@ -8,5 +8,5 @@ def convertRUBToUSD(amount):
     print('Стоимость одного доллара по данным ресурса finance.yahoo составляет', USDPrice[0].text.strip(), 'рублей.') #сообщает стоимость одного USD
     print(amount, 'американских долларов составляет', round(result, 2), 'российских рублей.') #выводит итоговую сумму на экран с точностью до 2 символов после запятой
 
-amount = input('Введите сумму USD, которую необходимо конвертировать в RUB.') #Запрос пользователю на ввод сумму к конвертации
-convertRUBToUSD(amount) #Выполнение функции
+amount = input('Введите сумму USD, которую необходимо конвертировать в RUB.\n') #Запрос пользователю на ввод сумму к конвертации
+convertUSDToRUB(amount) #Выполнение функции
